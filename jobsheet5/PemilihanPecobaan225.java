4a29ca946ec06e816e295d70914412f4740737ec
Sadiya8924 <sadiyamaritza8924@gmail.com>
2023-09-28 12:50:12 +0700

    jobsheet5

---
+++ jobsheet5/PemilihanPecobaan225.java
@@ -0,0 +1,19 @@
+/**
+ * PemilihanPecobaan225
+ */
+import java.util.Scanner;
+public class PemilihanPecobaan225 {
+
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        System.out.print("Nilai uas   : ");
+        float uas = input25.nextFloat();
+        System.out.print("Nilai uts : ");
+        float uts = input25.nextFloat();
+        System.out.print("Nilai kuis    : ");
+        float kuis = input25.nextFloat();
+        System.out.print("Nilai tugas   : ");
+        float tugas = input25.nextFloat();
+        
+    }
+}
---
+++ jobsheet5/PemilihanPercobaan125.java
@@ -0,0 +1,18 @@
+/**
+ * PemilihanPercobaan125
+ */
+import java.util.Scanner;
+public class PemilihanPercobaan125 {
+
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input25.nextInt();
+        if (angka % 2 == 0)
+            System.out.println("Angka "+angka+" bilangan genap");
+        else
+            System.out.println("Angka "+angka+" bilangan ganjil");        
+
+
+    }
+}
