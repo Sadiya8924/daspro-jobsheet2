8f1b63727f993a87706a050c5507780d12a4e3c5
Sadiya8924 <sadiyamaritza8924@gmail.com>
2023-10-27 04:56:08 +0700

    jobsheet8

--- jobsheet8/ArrayRataNilai24.java
+++ jobsheet8/ArrayRataNilai24.java
@@ -9,6 +9,10 @@ public class ArrayRataNilai24 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        double rataLulus = 0;
+        double rataTidakLulus = 0;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -16,8 +20,29 @@ public class ArrayRataNilai24 {
         }
         for (int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            } else {
+                rataTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            } 
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata rata nilai = "+rata2);
+
+        if (jumlahLulus > 0) {
+            rataLulus /= jumlahLulus;
+        }
+        if (jumlahTidakLulus > 0) {
+            rataTidakLulus /= jumlahTidakLulus;
+        }
+
+        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus = " + jumlahTidakLulus);
+        if (jumlahLulus > 0) {
+            System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        }
+        if (jumlahTidakLulus > 0) {
+            System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        }
     }
 }
