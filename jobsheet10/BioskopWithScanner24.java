745056a132cbc4a0df326ee1ac64608e18fbb3d7
Sadiya8924 <sadiyamaritza8924@gmail.com>
2023-11-03 06:01:01 +0700

    jobsheet10

--- jobsheet10/BioskopWithScanner24.java
+++ jobsheet10/BioskopWithScanner24.java
@@ -6,6 +6,25 @@ public class BioskopWithScanner24 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        String nama, next;
+        int baris, kolom;
+        String[][] penonton = new String[4][2];
+
+        while (true){
+            System.out.print("Masukkan nama: ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan baris: ");
+            baris = sc.nextInt();
+            System.out.println("Masukkan kolom: ");
+            kolom = sc.nextInt();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.print("input penonton lainnya? (y/n): ");
+            next = sc.nextLine();
+            if (next.equalsIgnoreCase("n")){
+                break;
+            }
+        }
     }
 }
