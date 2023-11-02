ea4f8fe5869da205523f6bbe6fa6e60bd501c5f5
Sadiya8924 <sadiyamaritza8924@gmail.com>
2023-11-03 05:31:42 +0700

    jobsheet10

--- jobsheet10/Bioskop24.java
+++ jobsheet10/Bioskop24.java
@@ -19,10 +19,13 @@ public class Bioskop24 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.out.println("Penonton baris ke-3: ");
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " +barisPenonton.length);
+        }
 
-        for (String i : penonton[2]){
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Penonton pada baris ke-" +(i+1)+": "+ String.join(", ", penonton[i]));
         }
     }
 }
