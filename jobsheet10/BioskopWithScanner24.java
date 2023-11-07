9eac7ecf212bd946bea56785007243c0eb41ec6e
Sadiya8924 <sadiyamaritza8924@gmail.com>
2023-11-07 20:24:13 +0700

    jobsheet11

--- jobsheet10/BioskopWithScanner24.java
+++ jobsheet10/BioskopWithScanner24.java
@@ -7,10 +7,24 @@ public class BioskopWithScanner24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, next;
-        int baris, kolom;
+        int baris, kolom, menu;
         String[][] penonton = new String[4][2];
 
-        while (true){
+        do {
+            System.out.println("/t Menu");
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Daftar Penonton");
+            System.out.print("Masukkan Nomer Menu: ");
+            menu = sc.nextInt();
+            if (menu == 3 ) {
+            break;
+        }
+        switch (
+            menu) {
+            case 1:
+            while (true) {
+            System.out.println("Input Data Penonton");
+            sc.nextLine();
             System.out.print("Masukkan nama: ");
             nama = sc.nextLine();
             System.out.print("Masukkan baris: ");
@@ -24,9 +38,40 @@ public class BioskopWithScanner24 {
             System.out.print("input penonton lainnya? (y/n): ");
             next = sc.nextLine();
 
-            if (next.equalsIgnoreCase("n")){
-                break;
+            if(baris<1 || baris> penonton.length || kolom < 1 || kolom >
+                penonton[0].length){
+                System.out.println("Nomor Baris/Kolom Kursi Tidak Tersedia");
+            continue;
             }
+            if(penonton[baris-1][kolom-1] != null){
+                System.out.println("Kursi telah terisi, masukkan baris dan kolom kembali.");
+            continue;
+            }else{
+                penonton[baris-1][kolom-1] = nama;
+            }
+                System.out.print("Input penonton lainnya? (y/n): ");
+                next = sc.nextLine();
+            if(next.equalsIgnoreCase("n")){
+            break;
+                }
+            }
+                break;//break menu1
+            case 2:
+                System.out.println("===Tampilkan Daftar Penonton===");
+            for (int i = 0; i < penonton.length; i++) {
+            for (int j = 0; j < penonton[i].length; j++) {
+                if  (penonton[i][j] != null) {
+                    System.out.println("Penonton pada baris " + (i+1) + ", kolom " + (j+1) + ": " + penonton[i][j]);
+                } else {
+                    System.out.println("Penonton pada baris " + (i+1) + ", kolom " + (j+1) + ": ***");
+                }
+            }   
         }
+            break;//break menu2
+            default:
+                System.out.println("Masukkan Nomer yang ada pada menu!!!");
+            break; //break def
+            } 
+        } while( menu != 3);
     }
 }
---
+++ jobsheet11/Star.java
@@ -0,0 +1,17 @@
+/**
+ * Star
+ */
+import java.util.Scanner;
+public class Star {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        for (int i = 0; i <= N; i++){
+            System.out.print("*");
+        }
+    }
+}
